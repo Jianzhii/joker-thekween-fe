659f5454c1ef36698835c8fe8b15c1b812822b0c
Jianzhii <60025391+Jianzhii@users.noreply.github.com>
2022-03-14 21:21:56 +0800

    first commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-southeast-1_FIZne00q4', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2gob0c8744aql2q60gn1n30dk9', // e.g. 7co724p0r8npg1u6hfmd1acnee 
+        region: 'ap-southeast-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
